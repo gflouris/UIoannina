9e04c33a5ce7e53a5b6bf4563256c333e69eb123
gflouris <flourisj@gmail.com>
2014-10-30 16:43:38 +0200

    read and plot

---
+++ TrUpS/macros/reader_plotter.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from xml.dom import minidom
+from ROOT import gROOT, TCanvas, TH1F
+gROOT.Reset()
+
+histo1 = TH1F("histo1","histo1",100,-1,1)
+
+doc = minidom.parse("output.xml")
+print
+events = doc.getElementsByTagName("Event")
+k = errors = 0
+n_events = 1700
+pt_tr_emu=[]
+pt_DTTF=[]
+resolution = 0
+
+for event in events:
+	resolution=0
+	if int(event.getAttribute("id")) < n_events:
+		infos = event.getElementsByTagName("info")
+		for info in infos:
+			if info.getAttribute("type") == "tr_emu":
+				pt_tr_emu.append(info.getElementsByTagName("pt")[0].firstChild.data)
+
+			if info.getAttribute("type") == "DTTF":
+				pt_DTTF.append(info.getElementsByTagName("pt")[0].firstChild.data)
+			    
+				resolution = (1./float(pt_tr_emu[k]) - 1./float(pt_DTTF[k])) / (1./float(pt_tr_emu[k]))
+				histo1.Fill(resolution)
+				#print("%d  %s  %s" %(k,pt_tr_emu[k],pt_DTTF[k]))
+
+		k=k+1
+
+ 
+c1 = TCanvas( 'c1', 'Resolution', 200, 10, 700, 500 )
+#fun1 = TF1( 'fun1', 'abs(sin(x)/x)', 0, 10 )
+c1.SetGridx()
+c1.SetGridy()
+histo1.Draw()
+c1.Update()
+
+raw_input("Press any key to exit.")
